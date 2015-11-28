ae38101a720c046a37af5cddf12eca43a1d64787
Diego Guajardo <diego.guajardo@hotmail.cl>
2015-11-28 16:05:15 -0300

    Machine-Supply association done

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,2 +1,4 @@
 class Machine < ActiveRecord::Base
+has_many :requests, through: :supplies
+has_many :supplies,through: :requests
 end
--- app/models/supply.rb
+++ app/models/supply.rb
@@ -1,2 +1,4 @@
 class Supply < ActiveRecord::Base
+has_many :requests
+has_many :machines, through: :requests
 end
